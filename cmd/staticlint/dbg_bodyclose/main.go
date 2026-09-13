20f2080121082860d643ddbe14499def3165dd6b
KonstantinDB <konstantin.duvakin-87@yandex.ru>
2026-09-13 19:39:43 +0500

    iteration 21 first commit

--- cmd/staticlint/dbg_bodyclose/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-	"github.com/timakin/bodyclose/passes/bodyclose"
-	"golang.org/x/tools/go/analysis/multichecker"
-)
-
-func main() { multichecker.Main(bodyclose.Analyzer) }
--- cmd/staticlint/dbg_simple/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-	"golang.org/x/tools/go/analysis/multichecker"
-	"honnef.co/go/tools/simple"
-)
-
-func main() { multichecker.Main(simple.Analyzers[0].Analyzer) }
